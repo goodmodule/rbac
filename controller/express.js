46a40af973a9e631acdf71affa3e008457cb65f6
Zlatko Fedor <zfedor@gmail.com>
2014-04-08 17:22:30 +0200

    added type control for controller

--- controller/express.js
+++ controller/express.js
@@ -1,11 +1,18 @@
 'use strict';
 
+/**
+ * Return middleware function for permission check
+ * @param  {RBAC}    rbac      Instance of RBAC
+ * @param  {String}  action    Name of action
+ * @param  {String}  resource  Name of resource
+ * @return {Function}          Middleware function
+ */
 exports.isAllowed = function(rbac, action, resource) {
 	var permission = rbac.getPermission(action, resource);
 	if(!permission) throw new Error('Permission is undefined');
 
 	return function(req, res, next) {
-		if(req.isAllowed && req.isAllowed(permission)) {
+		if(req.isAllowed && req.isAllowed(permission) === true) {
 			next();
 		}
 
@@ -13,12 +20,18 @@ exports.isAllowed = function(rbac, action, resource) {
 	};
 };
 
+/**
+ * Return middleware function for permission check
+ * @param  {RBAC}  rbac     Instance of RBAC
+ * @param  {String}  name   Name of role
+ * @return {Function}       Middleware function
+ */
 exports.hasRole = function(rbac, name) {
 	var role = rbac.getRole(name);
 	if(!role) throw new Error('Role is undefined');
 
 	return function(req, res, next) {
-		if(req.hasRole && req.hasRole(role)) {
+		if(req.hasRole && req.hasRole(role) === true) {
 			next();
 		}
 
